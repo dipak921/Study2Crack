7cf5b5371ddbfe903fa4abbc8c02187a2d48e545
dipak921 <dipakchande921@gmail.com>
2026-04-05 18:35:55 +0530

    E-Learning Project

--- ELearningPlatform/obj/Debug/net9.0/ELearningPlatform.AssemblyInfo.cs
+++ ELearningPlatform/obj/Debug/net9.0/ELearningPlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68fad236229331e7121f9da8fc924e73b5f98ff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
